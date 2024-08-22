bdac05dfcd3cb4c0d47950841988271604403014
duy <duy211099@gmail.com>
2024-08-22 16:13:33 +0700

    config: setup git pre-commit hook to format code

--- config/config.exs
+++ config/config.exs
@@ -61,6 +61,16 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :git_hooks,
+  verbose: true,
+  hooks: [
+    pre_commit: [
+      tasks: [
+        "mix format"
+      ]
+    ]
+  ]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
--- mix.exs
+++ mix.exs
@@ -59,7 +59,8 @@ defmodule BaselinePhoenix.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:git_hooks, "~> 0.3.0", only: :dev, runtime: false}
     ]
   end
 
