31508f77bb25836ed540f3a873e84dcc8af769eb
duy <duy211099@gmail.com>
2024-08-31 01:20:02 +0700

    feat: auth

---
+++ lib/baseline_phoenix_web/controllers/user_reset_password_html.ex
@@ -0,0 +1,5 @@
+defmodule BaselinePhoenixWeb.UserResetPasswordHTML do
+  use BaselinePhoenixWeb, :html
+
+  embed_templates "user_reset_password_html/*"
+end
---
+++ lib/baseline_phoenix_web/controllers/user_settings_html.ex
@@ -0,0 +1,5 @@
+defmodule BaselinePhoenixWeb.UserSettingsHTML do
+  use BaselinePhoenixWeb, :html
+
+  embed_templates "user_settings_html/*"
+end
