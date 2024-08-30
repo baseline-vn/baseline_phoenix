95d876084d4c4f0a6dcf205532cb2964a07324a0
duy <duy211099@gmail.com>
2024-08-31 02:54:45 +0700

    clean up

--- lib/baseline_phoenix_web/controllers/user_reset_password_html.ex
+++
@@ -1,5 +0,0 @@
-defmodule BaselinePhoenixWeb.UserResetPasswordHTML do
-  use BaselinePhoenixWeb, :html
-
-  embed_templates "user_reset_password_html/*"
-end
--- lib/baseline_phoenix_web/controllers/user_settings_html.ex
+++
@@ -1,5 +0,0 @@
-defmodule BaselinePhoenixWeb.UserSettingsHTML do
-  use BaselinePhoenixWeb, :html
-
-  embed_templates "user_settings_html/*"
-end
