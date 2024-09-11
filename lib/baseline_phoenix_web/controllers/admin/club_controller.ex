21895b615b7c6f95a450d31db0f06469a65ceda8
ngocan120803@gmail.com <ngocan120803@gmail.com>
2024-09-11 16:01:10 +0700

    feat: show all clubs in admi page

--- lib/baseline_phoenix_web/controllers/admin/club_controller.ex
+++ lib/baseline_phoenix_web/controllers/admin/club_controller.ex
@@ -1,7 +1,11 @@
 defmodule BaselinePhoenixWeb.Admin.ClubController do
   use BaselinePhoenixWeb, :controller
 
+  alias BaselinePhoenix.Club
+  alias BaselinePhoenix.Repo
+
   def index(conn, _params) do
-    render(conn, :index)
+    clubs = Repo.all(Club)
+    render(conn, :index, clubs: clubs)
   end
 end
